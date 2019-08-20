8aeacace8fe3f77e85b4df2eaea9b32af6b17e28
Sara Mousavi <mousavi@kavir.eecs.utk.edu>
2019-08-19 20:41:07 -0500

    cluster based on days

--- cluster_embedings.py
+++ cluster_embedings.py
@@ -10,10 +10,10 @@ import datetime
 
 embedings_file = sys.argv[1]
 
-num_clusters = 3
+num_clusters = 2
 colors = [np.random.rand(3,) for i in range(num_clusters)]
 donors2imgs = {}
-donors2embed = {}
+donors2img2embed = {}
 
 def sort_dates(donors2imgs):
     print(donors2imgs)
@@ -31,22 +31,50 @@ def convert_to_time(img_name):
     date = date.replace('_', '') # to remove the '_'
     return datetime.datetime.strptime(date, '%m%d%y') #formated as date
 
-def cal_day_from_deth(donors2imgs):
-    for key in donors2imgs:
-        img2day = {}
+def cal_day_from_deth(donors2imgs_sorted):
+    for key in donors2imgs_sorted:
+        day2imgs = {} 
         first_img = True
-        for img in donors2imgs[key]:
+        for img in donors2imgs_sorted[key]:
             if first_img == True:
                 start_time = convert_to_time(img)
                 first_img = False
             img_time = convert_to_time(img)
             time_from_start = (img_time - start_time).days
-            img2day[img] = time_from_start
-        donors2imgs[key] = img2day 
-    print(donors2imgs)
-    return donors2imgs
+            if time_from_start not in day2imgs:
+                day2imgs[time_from_start] = []
+            day2imgs[time_from_start].append(img)
+        donors2imgs_sorted[key] = day2imgs 
+    return donors2imgs_sorted 
+    # this a dictionary with each donor_id as keys and values are another 
+    #dictionary with keys being xth days since day one and the values are a 
+    #list of images that belong to day xth for that donor.
 
-                
+def cluster(donor2img2embeding, donor2day2img):
+    for donor in donor2img2embeding:
+        embedings_per_day_per_donor = [i for i in range(len(donor2day2img[donor]))] #how many days per donor
+        for day in donor2day2img[donor]: #iterate through each day per donor
+            embedings_per_day_per_donor[day] = donor2day2img[donor][day] # only to sort days
+        for day in embedings_per_day_per_donor:#each day would include a list of images
+            day_vector = []
+            for img in day:
+                day_vector.append(donor2img2embeding[donor][img])
+
+            ## cluster 
+            vectors = np.array(day_vector)
+            kmeans = KMeans(n_clusters = num_clusters)
+            kmeans.fit(vectors)
+            labels = kmeans.predict(vectors)
+
+            ## Reduce dimention to visualize
+            model = TSNE(n_components=2, perplexity=40)
+            results = model.fit_transform(vectors)
+
+            plt.figure(figsize=(8,5))
+            for row_number in range(0, results.shape[0]):
+                plt.scatter(results[row_number,0]*100, results[row_number,1]*100, c = colors[labels[row_number]])
+            plt.show()
+            
 
 with open(embedings_file, 'r') as csv_file:
     data = csv.reader(csv_file,delimiter = '\n')
@@ -59,36 +87,20 @@ with open(embedings_file, 'r') as csv_file:
         embeding = embeding.replace(' ', ',')
         embeding = ast.literal_eval("[" + embeding[1:-1] + "]") # this embeding is a list now
         donor_id = img_name.split('/Daily')[0].split('/')[-1]
-        if donor_id not in donors2embed and donor_id not in donors2imgs:
-            print(donor_id)
-            donors2embed[donor_id] = {} 
+        if donor_id not in donors2img2embed and donor_id not in donors2imgs:
+            donors2img2embed[donor_id] = {} 
             donors2imgs[donor_id] = []
-        donors2embed[donor_id][img_name] = embeding
+        donors2img2embed[donor_id][img_name] = embeding 
+        # this a dictionary with each donor_id as keys and values are another dictionary
+        # with keys being an image and the values being the feature vector for that imag
+
         donors2imgs[donor_id].append(img_name)
 
-        #vectors.append(embeding)
-    #vectors = np.array(vectors)
-    donors2imgs2 = sort_dates(donors2imgs)
-    dates = cal_day_from_deth(donors2imgs2)
-    import bpython
-    bpython.embed(locals())
-    exit()
+    donors2imgs_sorted = sort_dates(donors2imgs)
+    donor2day2imgs = cal_day_from_deth(donors2imgs_sorted)
+    cluster(donors2img2embed, donor2day2imgs)
 
-
-    ## cluster 
-    kmeans = KMeans(n_clusters = num_clusters)
-    kmeans.fit(vectors)
-    labels = kmeans.predict(vectors)
-    
-    ## Reduce dimention to visualize
-    model = TSNE(n_components=2, perplexity=40)
-    results = model.fit_transform(vectors)
-    
-    plt.figure(figsize=(8,5))
-    for row_number in range(0, results.shape[0]):
-        plt.scatter(results[row_number,0]*100, results[row_number,1]*100, c = colors[labels[row_number]])
-    plt.show()
-
-    
+    ''' 
     for index, label in enumerate(labels):
-        print(donors2imgs[index] , ":", label)
+         print(donors2imgs[index] , ":", label)
+    '''
